5470dbd9166dc5c8bb10ec4a143fa757e3e9a4ea
Codigo Estudiante <jchambi97@hotmail.com>
2025-05-21 16:29:34 -0500

    first commit

---
+++ DemoVerticalSlice/ContextDB/ConnectionDB.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace DemoVerticalSlice.ContextDB;
+
+public class ConnectionDB(IConfiguration _config)
+{
+    public SqlConnection GetSQL()
+    {
+        return new SqlConnection(_config.GetConnectionString("sqlString"));
+    }
+
+}
---
+++ DemoVerticalSlice/Features/Products/GetAllProduct.cs
@@ -0,0 +1,58 @@
+﻿using DemoVerticalSlice.ContextDB;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace DemoVerticalSlice.Features.Products
+{
+
+    //request
+    public record GetAllProductQuery();
+
+    //Response
+    public record GetAllProductResponse(int IdProduct,string Name, int Price);
+
+    //Handler
+    public class GetAllProductHandler(ConnectionDB _connectionDB)
+    {
+        public List<GetAllProductResponse> Handle(GetAllProductQuery value)
+        {
+
+
+            var products = new List<GetAllProductResponse>();
+
+            using (var cn = _connectionDB.GetSQL())
+            {
+                cn.Open();
+                var command = new SqlCommand("select IdProduct,Name,Price from Product", cn);
+                command.CommandType = System.Data.CommandType.Text;
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        products.Add(new GetAllProductResponse(
+                            int.Parse(reader["IdProduct"].ToString()!),
+                            reader["Name"].ToString()!,
+                            int.Parse(reader["Price"].ToString()!)
+                            ));
+                    }
+                }
+            }
+            return products;
+        }
+    }
+
+    //EndPoint
+    [ApiController]
+    [Route("api/product")]
+    public class GetAllProductController(ConnectionDB _connectionDB): ControllerBase
+    {
+        [HttpGet]
+        [Route("getAll")]
+        public IActionResult GetAll() { 
+            var query = new GetAllProductQuery();
+            var products = new GetAllProductHandler(_connectionDB).Handle(query);
+            return Ok(products);
+        }
+    }
+}
